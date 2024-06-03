85ddc3ab6af2977553735aca317d5d07cfff2934
Aquiles Duarte <166404816+Aquiiiles@users.noreply.github.com>
2024-06-03 19:24:56 -0300

    third commit

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ def mm_to_p(mm):
 
 pdf = canvas.Canvas('./exemplo.pdf', pagesize=A4)
 
-pdf.setFont('Helvetica-Oblique',14)
+pdf.setFont('Helvetica-Bold',18)
 pdf.drawString(mm_to_p(20), mm_to_p(50), 'Hello world, welcome to my new project')
+
+pdf.setFont('Helvetica-Oblique', 14)
+pdf.drawString(mm_to_p(20), mm_to_p(40), 'this is my first project in python')
+
+pdf.circle(mm_to_p(50), mm_to_p(80), mm_to_p(20))
+pdf.rect(mm_to_p(100), mm_to_p(100), mm_to_p(70), mm_to_p(40))
 
 pdf.save()
